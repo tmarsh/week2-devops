91cd4dc4e87e5527de0676657d63f13483482815
Tim Marsh <timothy.marsh@stratus10.com>
2023-06-25 08:10:40 -0700

    build didn't work specifying the img did

--- quote_disp/app.py
+++ quote_disp/app.py
@@ -18,11 +18,11 @@ def home():
 
 @app.route("/get_quote")
 def quote():
-    quote = requests.get("http://quote-gen-container:5000/quote").text
+    quote = requests.get("http://week2-devops-web1-1:5000/quote").text
     print("quote - ", quote)
 
     return render_template("quote.html", quote=quote)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5001)
+    app.run(host="0.0.0.0", debug=True, port=5010)
